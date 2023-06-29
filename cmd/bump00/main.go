e3083afa2ba04a1e95425d5e8627acc72f67bb9f
Hau Nguyen <hauvipapro@gmail.com>
2023-06-29 18:27:45 +0700

    feat: bump00 skeleton

---
+++ cmd/bump00/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+    // TODO
+    // git fetch latest tags
+    // if tags has RC -> bump RC
+    // otherwise bumpversion with RC1
+}
